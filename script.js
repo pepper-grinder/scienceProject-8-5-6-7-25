ef575480e2461d10d986cef2df0b7808d5c19542
pepper-grinder <94027236+pepper-grinder@users.noreply.github.com>
2025-06-07 00:18:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("linked");
 
 function a(){
-    window.location.href = "Basil.html";
+    window.location.href = "https://pepper-grinder.github.io/scienceProject-8-5-6-7-25/Basil.html";
 }
